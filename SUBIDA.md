b04d3dcca32ff8cf8b3df8aac9e01e27b37a23a7
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2025-09-28 19:42:37 +0200

    primer programa

--- SUBIDA.md
+++
@@ -1,2 +0,0 @@
-# PROYECTO 1
-Este es un ejemplo de modificación para practicar en las asignaturas de Programación y MOP
---
+++ programa_uno.java
@@ -0,0 +1,5 @@
+public class programa_uno {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
