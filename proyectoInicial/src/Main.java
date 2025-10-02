84a60ed8ac408b6fc59b9eb44a25c239f58d0387
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2025-10-02 19:50:06 +0200

    update new class and method

--- proyectoInicial/src/Main.java
+++ proyectoInicial/src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
 
-    
+    public static void main(String[] args) {
+        System.out.println("Este es mi primer programa en Java");
+        System.out.println("Este es mi segunda línea del programa en Java");
+    }
+
+    public void nombreMetodo() {
+
+
+    }
 }
